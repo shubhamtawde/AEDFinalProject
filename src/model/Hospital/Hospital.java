7c5baa6c90a4d69f10c236c67596e80e6bd7422b
Shubham Tawde <tawde.s@northeastern.edu>
2022-12-10 18:43:23 -0500

    added Hospital model

---
+++ src/model/Hospital/Hospital.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.Hospital;
+
+/**
+ *
+ * @author shubham
+ */
+public class Hospital {
+    
+    private String hospitalId;
+    
+    private String hospitalName;
+    
+    private String hospitalCity;
+
+    public String getHospitalId() {
+        return hospitalId;
+    }
+
+    public void setHospitalId(String hospitalId) {
+        this.hospitalId = hospitalId;
+    }
+
+    public String getHospitalName() {
+        return hospitalName;
+    }
+
+    public void setHospitalName(String hospitalName) {
+        this.hospitalName = hospitalName;
+    }
+
+    public String getHospitalCity() {
+        return hospitalCity;
+    }
+
+    public void setHospitalCity(String hospitalCity) {
+        this.hospitalCity = hospitalCity;
+    }
+
+    @Override
+    public String toString() {
+        return hospitalId;
+    }
+    
+    
+    
+}

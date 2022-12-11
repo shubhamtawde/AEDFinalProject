a9d03ab552e9ae0f0cf4ab2483b8ce5a2b0ed905
siddharthpvaghela <vaghela.s@northeastern.edu>
2022-12-11 01:08:30 -0500

    research admin logic

--- src/ui/ResearchAdmin.java
+++ src/ui/ResearchAdmin.java
@@ -174,6 +174,8 @@ public class ResearchAdmin extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+                MonitorResearcher monitorResearcher = new MonitorResearcher();
+        jSplitPane1.setRightComponent(monitorResearcher);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**

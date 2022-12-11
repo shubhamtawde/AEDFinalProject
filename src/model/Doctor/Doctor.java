7dbf69cd02ca74e37e05acf738c5bd0df93186a7
Shubham Tawde <tawde.s@northeastern.edu>
2022-12-11 12:52:58 -0500

    added Doctor model

---
+++ src/model/Doctor/Doctor.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.Doctor;
+
+/**
+ *
+ * @author shubham
+ */
+public class Doctor {
+    
+    private Long doctorId;
+    
+    private String doctorName;
+    
+    private String hospitalName;
+
+    public Long getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(Long doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getDoctorName() {
+        return doctorName;
+    }
+
+    public void setDoctorName(String doctorName) {
+        this.doctorName = doctorName;
+    }
+
+    public String getHospitalName() {
+        return hospitalName;
+    }
+
+    public void setHospitalName(String hospitalName) {
+        this.hospitalName = hospitalName;
+    }
+
+    @Override
+    public String toString() {
+        return doctorName;
+    }
+    
+    
+    
+}

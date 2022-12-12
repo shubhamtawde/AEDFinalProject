ed20e9fd6be18fda0bb31ff559dfff2c9cdcfdac
vidhisejpal <114622705+vidhisejpal@users.noreply.github.com>
2022-12-11 19:46:13 -0500

    created credentials model

---
+++ src/Model/Credentials/Credentials.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.Credentials;
+
+/**
+ *
+ * @author vidhisejpal
+ */
+public class Credentials {
+    
+    private Long id;
+    
+    private String username;
+    
+    private String password;
+    
+    private String role;
+    
+    private String license;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    @Override
+    public String toString() {
+        return username;
+    }
+    
+    
+    
+}
